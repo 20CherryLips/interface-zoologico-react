56669cdd4773e202b47489895624d0f26f60fcd5
Rayssa Perdigão <rayssaperdigao20@gmail.com>
2024-03-06 16:32:59 -0300

    Criando pagina Animais

---
+++ src/components/pages/Animais/Animas.jsx
@@ -0,0 +1,14 @@
+import CardAnimal from "../../CardAnimal/CarAnimal";
+import CardAnimal from "../../CardAnimal/CarAnimal";
+import './Animais.css'
+
+function Animais() {
+    return(
+        <>
+            <Navegcao/>
+            <CardAnimal/>
+        </>
+    );
+}
+
+export default Animais
