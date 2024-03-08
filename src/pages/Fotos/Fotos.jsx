9c11543f80b7c3dfda6c02cd156a3ff248bcd1d1
Rayssa Perdigão <rayssaperdigao20@gmail.com>
2024-03-08 13:38:38 -0300

    As paginas Fotos, Depoimentos e Login foram criadas

---
+++ src/pages/Fotos/Fotos.jsx
@@ -0,0 +1,13 @@
+import Navegacao from '../../components/Navegacao/Navegacao';
+import './Fotos.css'
+
+function Animais() {
+    return(
+        <>
+            <Navegacao />
+            <p>Nada aqui ainda</p>
+        </>
+    );
+}
+
+export default Animais
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navegacao from '../../components/Navegacao/Navegacao';
+import './Login.css'
+
+function Animais() {
+    return(
+        <>
+        <Navegacao />
+        <p>Nada aqui ainda</p>
+    </>
+    );
+}
+
+export default Animais
